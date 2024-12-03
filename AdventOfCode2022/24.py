e1156e4658588b910110d244b482045b2ecc749d
Georgiy Riskov <georgri@wb.ru>
2024-12-03 11:27:03 +0300

    addded 3 to 2022

---
+++ AdventOfCode2022/24.py
@@ -0,0 +1,119 @@
+text = """
+#.########################################################################################################################
+#<^^<.^>v>^.^v>^v>v<<>^^>vv><^<v>>^v>.<vv^^^<^^^vv<v.v^>>^>^>^><^<>vv<^v>>^<^>vv^v.>.vv.<^..<^v>.<v.v<>v>>.v<><v<<v<><>v<#
+#<>.^>v>^<>^><<>vv>v<><>^<v<^v^vv.>><>vv>>vv<^<^<vv>><>vv<^<vvv><>v>>.<>..><^<.<.><<<>^v^>^.^..vv><^vvv^^><.^^^><>^v<>v<>#
+#<><^.<vv^^^v<>>><v><^.^>^><vvv^vv^><^><v<^<>>...<v>.^v>>v^.>^v^^>^>>v<v>>>v<vv^^^<>^<>><<^^^v<^^v^.<>.vv.>^^v^v^<>v>^v<<#
+#><.v^>vv^^<<><v<>^.<^<>^^>v.v<v>>>^>>v>><v.v>.>^^<^><><>>.^v^v<^<>..<<<>.>.^<v<<v><v<<>^>v<<v.v>^>v.><vv>>><^^>>>v<^^^^<#
+#>>.v.^v<^<^^><<v^<>.><v<>>>>>>v>v<v>><v>v><<<..>v>vv<>><^<^><>v^^>v<<<>v>.v>>.<<>>vvv^<<v<^^v^v^v>><<^vvv^^<<v^^^^v.<^v>#
+#<<^v^<^^>>^.v.>>v><.^<^<^<.v<v^v>>><v^><<^^<v^^vv^<<.v^^<v^><<>.<>><>v^><><v^v>^^v>><>vv>v<><^<>.<^^>>^.^.^v^^>>^<.^.^<<#
+#.>v.^>v<v^>.^>vvv..v>vv>><v<<v<^^><>>v<vv>v<<<<v<<^v<^^>>v<v<v^><<^>vv<vv<>>>^^v.^<.vvv>><v<>>vv>.^<^.v>^vvvv^>v<>^>>v.>#
+#<v<^.>>.>vv<vv^^<><<..^<vv>vv<<<<>>^v.v^vv^v>>>..vv>>^>v^<>>>.v.<vv>.v^>^>.<.v.^>.v.v<>>.vv><<.><<v>v><v>>>v<^^^>^<>v.v>#
+#><v^vv<^^^<^^>v^^<>.^>>^v^v^^<<vv<v^.<<>.><>><<^v^^><v^vv<v.>v<<>.vvv>v<<v^<<<^vv<.<<>><>>>^^vv^<.>^>^<>^>^<^.<<vv^^v><<#
+#>v.^<.^>v.><><<>>>^>>>>^.v><^^vvvvv^<.^><>^^><^<^^^><><v<^<v>v^>^.^^><v>vv.>><<<.vvvv<<<.<^v^.<v..><^^<^v^>><^^<^v>.>v<.#
+#>vvv.<>v.v.><<v^<>>v<v><.>^><>^>.v^>v>v><^^v<^v.v^><><>>v<^<v>><<<^v<^^^^><>.>..^>vv><>vvv^^<>vv^>><v^^.v>^>v^<<>>>vv.^>#
+#<<v<>v^<vv.><v.^vv.<<^<<^<^<><vv<^^>v.<^><v.v<v.v>>>vv^>^^.<v<^...>..^^<^>><^v>>vv.>.^vvv<>^^>v<.<v<^>>^^><><>v><^<^v>^>#
+#<>vvv^><^<<^.>v^^^><<.><.v^>^^>><vv<<>v<<.v><v^^>^>.v^>v>><v>^v^^^<v>.>^v<><><>vv^<v^^.^>^^<.>^v>.<<v<v<^>>^^<.<<<v^^v^<#
+#>>>^<<^v^^<v^.<>>v^^^>v^v^<.^><>^<><^v^<<<>^<v^>v>v<^v<^>>vv.v^^v<v>><vvvvvv^>><<v>.>.v^v<^v>>>>^>><v^^.^>>vv.vv>>>^^.^>#
+#>>>v<<.v^<^><^>v><<vv^>^.v<^<^v<.v<>.^^v>v^.v<vv>v>v.^<^v<<<^<v>>><<<^.<v.^^<v>>^v^v.^^^.>v^.v>>>v^<^^v>v^^>^.>^<.>^vvv<#
+#<<^>.v<<vv..^<vv^^<v^v>>v>.^v.<>>>vv>^.v<v<.v^><<v^^v.>><.v<^v.<^^>>>^v>>^.v<vv<>v>><.^^^^<vv>v^.<.vv<^>vvv>..v.<v><<v.<#
+#<<><v...>v^^^^v^><v^<>^^>v<.<.>v<v>^>v<>>>>.v<v^^^><>.vv^v^<^vv.^<vv^>^.vv<v>^>^>^vv<^<^<^>^<.v<<^v>>>v>><^vvvv<v>v<^>^>#
+#.^<><<<><.^>>.v^<><<<^<^v>^v<><v>>vv^^>v.vv^>><^<^<.vv^>>>v<>>>v^^^<^v>>^^^<v<<^>>.vv^v.><<<^vv^^<..<>^^v^v>^.^><v<vv.>>#
+#<<<>.^vv>^<.v>>>^^.v<^^^v^>>^vvv><^><>.v^.vv<<^vv^<v<v>v<<v<v^v>^^><^^.v^<^v>.^v<^<v>>v<.^>^v<^v^.<>^v.><.<^^<<vv^v>^<><#
+#>>vv<v<>^v>^^<v><v<>v<.>>>v<v>>>>.><>>vv^<>^v>.v^vvv.v>>vv>v<>^^>><v<>^v.^><>^^>^^v>v^<v>^<>^<^^.>vv^^v><<vv<<^.<^.<.>v>#
+#<>><<<v<>^.<v<^<^><<<<^><vv>v<<>^><<v><>^<^v>^.^>><^^<<^^>>v^^<<...>^<v>><>^^>v<^><>.^<^<^<^<^v^<v^^>vv^^v^><^<vv>v>^vv>#
+#<<>.v<^.v^^v<<^v>>><v^<v><<<<v>vv.<><<^^<<^<<<><^vv^.^<>>v.>>>v^>vv>^v><>><<^><<^<^<><^<>v<v^^v><>>^v><<>v.>v<>vv><^>>>>#
+#>>^vv<>v<>v^<>vvv>v>^v>..<<v.^>v>><>^v<>v>><^vv^v<v>vvvv><><>v<^><<vv.^^..<^><^>vv>^^.>>vv>^v^v<^vv>^<^v^<>><>^>>^<<v<v<#
+#<>>^>>^.><.^v.^^v.v<^><^^^v^v.^vv<.>><vv><>^.^vvv^<vv^^vv<<>>>^^^>v<^<^^v>^^v>><vv>><>><<>^.<vv>v>v<<<vvv^^v>vv^<.>>^v><#
+#><>>^<<^<>>v<<<<v>^<v<v<v^^v^>^.<^>vv..<vv<<<<v>v.>.<><>><<><<<.^<^<^^><vvv<>.<.<^><v^v>.^<v<v>v^^><><^>^^><<><v.<v^v.v.#
+########################################################################################################################.#
+"""
+
+text_test = """
+#.######
+#>>.<^<#
+#.<..<<#
+#>v.><>#
+#<^v^^>#
+######.#
+"""
+
+def parse(text):
+    res = []
+    for line in text.split('\n'):
+        if not line:
+            continue
+        res.append(line)
+
+    blizzards = []
+    dirs = {'^': 0, '>': 1, 'v':2, '<': 3}
+    for i, row in enumerate(res):
+        for j, c in enumerate(row):
+            if c in '<^>v':
+                blizzards.append((i,j,dirs[c]))
+
+    start = res[0].index('.')
+    end = res[-1].index('.')
+
+    return res, blizzards, (0, start), (len(res)-1, end), len(res), len(res[0])
+
+print(parse(text_test))
+
+
+def simulate_blizzards(blizzards, n, m):
+    # 0 1 2 3 ... n-2 n-1
+    # # . . .      .   #
+    res = set()
+    dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)] # up, right, down, left
+    for x,y,d in blizzards:
+        xo, yo = dirs[d]
+        newx = ((x + xo) - 1) % (n-2) + 1
+        newy = ((y + yo) - 1) % (m-2) + 1
+        res.add((newx,newy, d))
+    return res
+
+def adj(field, new_blizzars, x,y, n,m):
+    for xo, yo in ((0,0),(1, 0), (0, 1), (-1, 0), (0, -1)):
+        if 0 <= x + xo < n and 0 <= y+yo < m and field[x+xo][y+yo] != '#' and ((x+xo),(y+yo)) not in new_blizzars:
+            yield x+xo, y+yo
+
+def simulate(field, blizzards, start, target, n, m):
+    sx,sy = start
+    tx,ty = target
+
+    occupied = set()
+    occupied.add((sx,sy))
+
+    for i in range(10000):
+        blizzards = simulate_blizzards(blizzards, n,m)
+        bset = set((a,b) for a,b,_ in blizzards)
+        #print(blizzards)
+        new_occupied = set()
+        for x,y in occupied:
+            for a,b in adj(field, bset, x,y, n,m):
+                #print(a,b)
+                new_occupied.add((a,b))
+        occupied = new_occupied
+        #print(i+1, occupied)
+        if (tx,ty) in new_occupied:
+            print(i+1, "steps to get to ", tx, ty)
+            break
+
+    return i+1, blizzards
+
+def run(text):
+    field, blizzards, start, target, n, m = parse(text)
+
+    res = 0
+
+    steps, blizzards = simulate(field, blizzards, start, target, n, m)
+    res += steps
+    steps, blizzards = simulate(field, blizzards, target, start, n, m)
+    res += steps
+    steps, blizzards = simulate(field, blizzards, start, target, n, m)
+    res += steps
+
+    print(res)
+
+    #print("bad luck")
+
+run(text)
